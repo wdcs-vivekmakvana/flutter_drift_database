088f9fae04415c6507c307181099ee18c2ca7baf
vivek <vivek.makvana@codezeros.com>
2025-02-05 16:16:43 +0530

    - Change flavor in Xcode

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Drift App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
